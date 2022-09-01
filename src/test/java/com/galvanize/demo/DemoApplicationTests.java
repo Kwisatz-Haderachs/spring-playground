cf4e83e6a28050718a35bb0fc948c92c55ab7b4e
kwisatz-haderachs <frcranmer@gmail.com>
2022-09-01 11:56:00 -0500

    same as previous

--- src/test/java/com/galvanize/demo/DemoApplicationTests.java
+++ src/test/java/com/galvanize/demo/DemoApplicationTests.java
@@ -9,20 +9,4 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
-/*
-@Test
-public void testPostMessageEndpoint() throws Exception {
-    this.mvc.perform(post("/messages"))
-            .andExpect(status().isOk())
-            .andExpect(content().string("POST to messages route"));
-}
-
-   @Test
-    public void testIndexEndpoint() throws Exception {
-        this.mvc.perform(get("/").accept(MediaType.TEXT_PLAIN))
-                .andExpect(status().isOk())
-                .andExpect(content().string("GET to index route"));
-    }
- */

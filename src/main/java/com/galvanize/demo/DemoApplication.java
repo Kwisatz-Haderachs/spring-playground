08311b502dc23cea8127bbe453bac42d2311e831
kwisatz-haderachs <frcranmer@gmail.com>
2022-09-01 11:55:11 -0500

    removed comments to notes

--- src/main/java/com/galvanize/demo/DemoApplication.java
+++ src/main/java/com/galvanize/demo/DemoApplication.java
@@ -5,16 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
-
-/*
-Start/Run: ./gradlew bootRun
-Exit/Stop: Crtl+c
-
-
- */

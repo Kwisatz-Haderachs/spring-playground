baf554c744680b9cbd4aa4d9331a9311f81de384
kwisatz-haderachs <frcranmer@gmail.com>
2022-09-06 14:11:10 -0500

    del

--- src/main/java/com/galvanize/demo/math/MathController.java
+++ src/main/java/com/galvanize/demo/math/MathController.java
@@ -1,12 +1,7 @@
 package com.galvanize.demo.math;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/math")
@@ -15,12 +10,10 @@ public class MathController {
     public MathController(MathService ms){
         this.ms  = ms;
     }
-
     @GetMapping("/pi")
     public String servePi(){
         return ms.getPi()+"";
     }
-
     @GetMapping("/calculate")
     public String calculate(@RequestParam String operation, @RequestParam int x, @RequestParam int y){
         return ms.operation(operation, x, y);
@@ -29,7 +22,6 @@ public class MathController {
     public String sum(@RequestParam MultiValueMap<String, String> nums) {
         return ms.sum(nums.get("n"));
     }
-
     @PostMapping("/volume/rectangle/{length}/{width}/{height}")
     public String volumeRec(@PathVariable int length, @PathVariable int width, @PathVariable int height) {
         return ms.volumeRectangular(length, width, height);

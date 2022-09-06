afb6a80b6edaed0a292892bda9fa2d3a9b2c40bc
kwisatz-haderachs <frcranmer@gmail.com>
2022-09-06 15:52:25 -0500

    refactor

--- src/main/java/com/galvanize/demo/DemoApplication.java
+++ src/main/java/com/galvanize/demo/DemoApplication.java
@@ -8,4 +8,17 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	static String clean(String identifier) {
+		char[] cl = identifier.replace(" ", "_").replace("-", "").toCharArray();
+		StringBuilder sb = new StringBuilder();
+		for (char c :cl) {
+			if(c == '\0'){
+				sb.append("CTRL");
+			} else if (Character.isAlphabetic(c)) {
+				sb.append(c);
+			}
+		}
+		return sb.toString();
+	}
 }
